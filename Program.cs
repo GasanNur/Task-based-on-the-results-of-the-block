2fec0a61e16471cd73ab8e606671cf8603eaa0d2
Gasan <1793959@mail.rum>
2023-03-26 19:14:12 +0300

    array bulkhead function

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@
 Console.Write("Введите количество элементов массива:");
 int m = Convert.ToInt32(Console.ReadLine());
 string[] stringArray = new string[m];
+//создаем функцию переборки массива которая не чего не вращает
+void array(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine();
+    }
+}
